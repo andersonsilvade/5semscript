557ff65d528dd8b4849537439b37b519dc9e23ca
andersonsilvade <andersonsilvade@gmail.com>
2014-10-02 08:31:39 -0300

    banco cadastrado

---
+++ Projeto/backend/appengine/routes/bdclassificacao.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from config.template_middleware import TemplateResponse
+from gaecookie.decorator import no_csrf
+from tekton import router
+from gaegraph.model import Node
+from google.appengine.ext import ndb
+from gaeforms.ndb.form import ModelForm
+from tekton.gae.middleware.redirect import RedirectResponse
+
+
+
+@no_csrf
+def form():
+    contexto={'salvar_path':router.to_path(salvar) }
+    return TemplateResponse(contexto)
+
+class Classificacao(Node):
+    pil = ndb.StringProperty(required=True)
+    equ = ndb.StringProperty(required=True)
+    cat = ndb.StringProperty(required=True)
+    pon = ndb.IntegerProperty(required=True)
+
+class ClassificacaoFormTable(ModelForm):
+    _model_class = Classificacao
+    _include = [Classificacao.pil,Classificacao.equ,Classificacao.cat,Classificacao.pon]
+
+class ClassificacaoForm(ModelForm):
+    _model_class = Classificacao
+    _include = [Classificacao.pil,Classificacao.equ,Classificacao.cat,Classificacao.pon]
+
+
+
+def salvar(_resp,**propriedades):
+    classificacao_form = ClassificacaoForm(**propriedades)
+    erros = classificacao_form.validate()
+    if erros:
+        contexto={'salvar_path':router.to_path(salvar),'erros':erros,'classificacao':classificacao_form }
+        return TemplateResponse(contexto,'bdclassificacao/form.html')
+
+    else:
+        classificacao=classificacao_form.fill_model()
+        classificacao.put()
+        return RedirectResponse(router.to_path(form))
+
+
+
+
+
+
+
---
+++ Projeto/backend/appengine/routes/classificacao/home.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import,unicode_literals
+from gaepermission.decorator import login_not_required
+from routes.bdclassificacao import *
+from routes.bdclassificacao import Classificacao
+from config.template_middleware import TemplateResponse
+from gaecookie.decorator import no_csrf
+from tekton import router
+from tekton.gae.middleware.redirect import RedirectResponse
+
+
+
+@login_not_required
+@no_csrf
+def index(categoria):
+    query = Classificacao.query(Classificacao.cat == categoria).order(-Classificacao.pon)
+    pilotos = query.fetch()
+
+    if categoria == "f1":
+        contexto = {'f1':pilotos}
+        return TemplateResponse(contexto,'classificacaof1s/home.html')
+    if categoria == "dtm":
+         contexto = {'dtm':pilotos}
+         return TemplateResponse(contexto,'classificacaodtms/home.html')
+    if categoria == "indy":
+         contexto = {'indy':pilotos}
+         return TemplateResponse(contexto,'classificacaoindys/home.html')
+    if categoria == "nascar":
+         contexto = {'nascar':pilotos}
+         return TemplateResponse(contexto,'classificacaonascars/home.html')
+
+
+
+
